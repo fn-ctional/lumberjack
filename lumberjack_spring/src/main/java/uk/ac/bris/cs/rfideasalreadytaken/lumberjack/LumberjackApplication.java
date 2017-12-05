89243d63234869bbe3e4483bd5ebc1554f8d6c8d
James Farrington <cf16496@my.bristol.ac.uk>
2017-12-05 13:08:08 +0000

    added blank spring maven project

---
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumberjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumberjackApplication.class, args);
+	}
+}
