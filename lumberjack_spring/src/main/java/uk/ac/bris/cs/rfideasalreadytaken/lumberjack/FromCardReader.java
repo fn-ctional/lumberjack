dce9546ea55e45f5fcccfdf28817dbc7a8132db4
Adam Beddoe <ab16301@my.bristol.ac.uk>
2018-01-19 14:24:56 +0000

    Added returnDevice method to FromCardReader interface.

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromCardReader.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromCardReader.java
@@ -3,4 +3,7 @@ package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
 public interface FromCardReader {
 
     boolean takeOutDevice(Device device, User user);
+
+    boolean returnDevice(Device device, User user);
+    
 }
