6aa5281a80a3f9887e402f0367ecc988d914bbac
James <james.farrington.2016@my.bristol.ac.uk>
2018-01-20 19:57:01 +0000

    Disabled login being required

---
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/MainController.java
@@ -0,0 +1,14 @@
+package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+public class WebController {
+
+    
+
+}
