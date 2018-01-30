42efa87b6b7647c3e2a52cb65122591983f07967
Jago <jtaylor4288@gmail.com>
2018-01-30 01:36:19 +0000

    A file can be specified to which the key presses are written

--- rfid_pi/main.cpp
+++ rfid_pi/main.cpp
@@ -1,13 +1,19 @@
 #include "Event.hpp"
 #include "Pipe.hpp"
+#include <iostream>
 
 using Event::toChar;
 
-int main() {
+int main(int argc, char **argv) {
+  if ( argc < 2 ) {
+    std::cerr << "No file specified" << std::endl;
+    return -1;
+  }
+
   auto source = Event::Source("/dev/input/event0");
   Event::Event event;
 
-  Pipe::Pipe pipe("foo");
+  Pipe::Pipe pipe(argv[1]);
 
   while( source.next(event) ) {
     if ( event.type == INPUT_PROP_DIRECT && event.value == EV_KEY ) {
