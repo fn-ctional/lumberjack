9ff6d57653f39eed1264a8f3c3af728b6650e527
Adam Beddoe <ab16301@my.bristol.ac.uk>
2018-02-07 16:23:06 +0000

    Started skeleton REST handler.

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/MainController.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/MainController.java
@@ -1,11 +1,15 @@
 package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.IOException;
 
 @Controller
 public class MainController extends WebMvcConfigurerAdapter {
@@ -29,5 +33,35 @@ public class MainController extends WebMvcConfigurerAdapter {
     public String download() {
         return "templates/download.html";
     }
+
+    @RequestMapping(value = "/devices", method = RequestMethod.PATCH, consumes = "application/json", produces = "text/plain")
+    @ResponseBody
+    public ResponseEntity changeDeviceState(@RequestBody String scanJSON) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Scan scan = mapper.readValue(scanJSON, Scan.class);
+
+
+            boolean successful = true;
+            if (successful) {
+                return ResponseEntity.status(200).body("Device " + scan.getDeviceID() + " successfully by " + scan.getUserID() + ".");
+            }
+            else if (!successful) { //for example
+                return ResponseEntity.status(403).body("User " + scan.getUserID() + " is not permitted to take out device " + scan.getDeviceID() + ".");
+            }
+
+
+        } catch (IOException e) {
+            return ResponseEntity.status(400).body("Bad request.");
+        }
+        // call changeDeviceState in model (which might exist in master, not sure)
+
+        //if unsuccessful:
+        //return ResponseEntity.status(403).body("Device has already been taken out.");
+        //return ResponseEntity.status(403).body("User not recognised.");
+        //return ResponseEntity.status(403).body("Device not recognised.");
+        return ResponseEntity.status(500).body("Unknown server error.");
+    }
+
 
 }
