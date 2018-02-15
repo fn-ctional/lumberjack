c9960ee33f0b4fe2e474a729a5a57bdd1c26741d
HTRobinson <35919112+HTRobinson@users.noreply.github.com>
2018-02-15 14:37:04 +0000

    Update FromFrontEnd.java

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromFrontEnd.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromFrontEnd.java
@@ -4,32 +4,15 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+public interface FromFrontEnd {
 
-public class FromFrontEnd extends Backend {
+    Map<Device,User> getDevicesOut();
 
-
-    void setUserMaxDevices(User user, int max){
-        user.deviceLimit = max;
-    }
+    void setUserMaxDevices(User user, int max);
 
     void setDeadline(Device device, Date date);
 
-    void setDeviceType(Device device, String type){
-        device.type = type;
-    }
+    void setDeviceType(Device device, String type);
 
-    boolean getAvailable(Device device){
-        return device.available;
-
-    }
-
-    boolean getCurrentlyAssigned(Device device){
-      return device.currentlyAssigned;
-
-    }
-
-    boolean studentTakeLaptop(Scan scan) {
-        String result = takeOutDevice(scan.getDevice(), scan.getUser());
-        if (result == successRemoval) return true;
-        else return false;
-    }
+    void getStatus(Device device);
+}
