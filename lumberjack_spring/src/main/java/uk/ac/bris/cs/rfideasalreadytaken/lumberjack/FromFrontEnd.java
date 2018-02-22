f92d589c02fde0e79ec4e786a79ba6178101dcb1
jdvfarrington <james.farrington.2016@my.bristol.ac.uk>
2018-02-22 18:43:55 +0000

    Commented what we need in FromFrontEnd.

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromFrontEnd.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/FromFrontEnd.java
@@ -6,9 +6,45 @@ import java.util.Map;
 
 public interface FromFrontEnd {
 
-    /* Functions we need for the front-end
+    /*
 
-    
+    ----------    Things we need for the front-end    ----------
+
+    - get single user details (including groups they are in)
+    - get multiple user details (e.g. in a list or something)
+    - get multiple user details filtered by group
+
+    - get single device details
+    - get multiple device details (e.g. in a list or something)
+    - get multiple device details filtered by group
+
+    - get single group details
+    - get multiple group details
+
+    - getting the log for a user
+    - getting the log for a device
+    - getting the log for a group/subset of users
+    - getting the log for a group/subset of devices
+    - getting the entire log (why not)
+
+    - get single rule (applied to groups)
+    - get all rules
+
+    - edit user details (single and batch)
+    - edit device details (single and batch)
+    - edit group details (e.g. add users too a group)
+    - edit rules
+
+    - delete certain logs (e.g. by device, user, time frame)
+    - delete users
+    - delete devices
+    - delete groups
+    - delete rules
+
+    - add users (single and batch)
+    - add devices (single and batch)
+    - add groups
+    - add rules
 
     */
 
