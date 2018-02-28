9601eb897748e3d9b5712a06dbe4df28bf1bdc6f
Adam Beddoe <ab16301@my.bristol.ac.uk>
2018-02-28 16:55:20 +0000

    Started to move testing to JUnit testing form

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplication.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import uk.ac.bris.cs.rfideasalreadytaken.lumberjack.data.ScanDTO;
 
 @SpringBootApplication
-public class LumberjackApplication implements CommandLineRunner{
+public class LumberjackApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(LumberjackApplication.class);
 
@@ -17,39 +17,4 @@ public class LumberjackApplication implements CommandLineRunner{
 		SpringApplication.run(LumberjackApplication.class, args);
 	}
 
-	@Override
-	public void run(String... strings) throws Exception {
-
-		Backend backend = new Backend();
-		backend.resetDatabase();
-		backend.insertTestCases();
-
-		ScanDTO scanDTO = new ScanDTO();
-		scanDTO.setUser("1314831486");
-		scanDTO.setDevice("1314831486");
-
-		log.info("ScanDTO User:");
-		log.info(backend.scanReceived(scanDTO) + "\n");
-
-		scanDTO.setUser("36109839730967812");
-		scanDTO.setDevice("36109839730967812");
-
-		log.info("ScanDTO User:");
-		log.info("ScanDTO Device not taken out:");
-		log.info(backend.scanReceived(scanDTO) + "\n");
-
-		scanDTO.setUser("457436545");
-		scanDTO.setDevice("457436545");
-
-		log.info("ScanDTO User:");
-		log.info(backend.scanReceived(scanDTO) + "\n");
-
-		scanDTO.setUser("23482364326842334");
-		scanDTO.setDevice("23482364326842334");
-
-		log.info("ScanDTO User:");
-		log.info("ScanDTO Device not taken out:");
-		log.info(backend.scanReceived(scanDTO) + "\n");
-		log.info(backend.scanReceived(scanDTO) + "\n");
-	}
 }
--- lumberjack_spring/src/test/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplicationTests.java
+++ lumberjack_spring/src/test/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplicationTests.java
@@ -2,15 +2,22 @@ package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LumberjackApplicationTests {
 
+    @Autowired
+    private MainController testController;
+
 	@Test
 	public void contextLoads() {
+        assertThat(testController).isNotNull();
 	}
 
 }
