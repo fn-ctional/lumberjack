1d8fd4cac1d49364163282a05dca63864f2cec36
Adam Beddoe <ab16301@my.bristol.ac.uk>
2018-03-13 12:34:11 +0000

    Moved database properties to external configuration file

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplication.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/LumberjackApplication.java
@@ -3,9 +3,19 @@ package uk.ac.bris.cs.rfideasalreadytaken.lumberjack;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
+@PropertySource({ "classpath:${envTarget:database}.properties" })
 public class LumberjackApplication implements CommandLineRunner {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+        propertySourcesPlaceholderConfigurer() {
+            return new PropertySourcesPlaceholderConfigurer();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LumberjackApplication.class, args);
