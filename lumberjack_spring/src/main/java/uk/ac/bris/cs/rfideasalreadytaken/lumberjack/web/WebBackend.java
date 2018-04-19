28c25d6b588e42c4a5670650e7c200a781554c44
Nick <ns16699@bristol.ac.uk>
2018-04-19 12:52:47 +0100

    Neatened frontendDatabase tests

--- lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/web/WebBackend.java
+++ lumberjack_spring/src/main/java/uk/ac/bris/cs/rfideasalreadytaken/lumberjack/web/WebBackend.java
@@ -62,24 +62,20 @@ public class WebBackend implements FromFrontEnd {
     @Autowired
     private DatabaseEmails databaseEmails;
 
-    //Tested
     public void insertUser(User user) throws SQLException {
         databaseUsers.insertIntoUsers(user);
     }
 
-    //Tested
     public void insertUsers(List<User> users) throws SQLException {
         for (User user : users) {
             insertUser(user);
         }
     }
 
-    //Tested
     public User getUser(String userID) throws SQLException {
         return databaseUsers.loadUser(userID);
     }
 
-    //Tested
     public List<User> getUsers() throws SQLException {
         List<User> users = new ArrayList<User>();
         PreparedStatement stmt = databaseConnection.getConnection().prepareStatement("SELECT * FROM Users");
@@ -96,69 +92,57 @@ public class WebBackend implements FromFrontEnd {
         return users;
     }
 
-    //Tested
     public void editUser(String userID, User newValue) throws SQLException {
         databaseUsers.updateUser(userID, newValue);
     }
 
-    //Tested
-    public void changeUserGroup(User user, UserGroup group) throws Exception{
+    public void changeUserGroup(User user, UserGroup group) throws Exception {
         user.setGroupId(group.getId());
         databaseUsers.updateUser(user.getId(), user);
     }
 
-    //Tested
-    public void changeUsersGroup(List<User> users, UserGroup group) throws Exception{
+    public void changeUsersGroup(List<User> users, UserGroup group) throws Exception {
         for (User user : users) {
             changeUserGroup(user, group);
         }
     }
 
-    //Tested
     public void setUserMaxDevices(User user, int max) throws Exception {
         user.setDeviceLimit(max);
         databaseUsers.updateUser(user.getId(), user);
     }
 
-    //Tested
     public void deleteUser(String userID) throws Exception {
         try {
             databaseUsers.deleteFromUsers(userID);
+        } catch (SQLException e) {
         }
-            catch (SQLException e)
-        {}
     }
 
-    //Tested
     public void resetUsers() throws SQLException {
         List<User> users = getUsers();
         for (User user : users) {
             try {
                 databaseUsers.deleteFromUsers(user.getId());
+            } catch (SQLException e) {
             }
-            catch (SQLException e)
-            {}
         }
     }
 
-    //Tested
     public void insertDevice(Device device) throws SQLException {
         databaseDevices.insertIntoDevices(device);
     }
 
-    //Tested
     public void insertDevices(List<Device> devices) throws SQLException {
         for (Device device : devices) {
             insertDevice(device);
         }
     }
 
-    //Tested
     public Device getDevice(String deviceID) throws SQLException {
         return databaseDevices.loadDevice(deviceID);
     }
 
-    //Tested
     public List<Device> getDevices() throws SQLException {
         List<Device> devices = new ArrayList<>();
         PreparedStatement stmt = databaseConnection.getConnection().prepareStatement("SELECT * FROM Devices");
@@ -175,50 +159,41 @@ public class WebBackend implements FromFrontEnd {
         return devices;
     }
 
-    //Tested
     public void editDevice(String deviceID, Device newValue) throws SQLException {
         databaseDevices.updateDevice(deviceID, newValue);
     }
 
-    //Tested
-    public void setDeviceType(Device device, String type) throws Exception{
+    public void setDeviceType(Device device, String type) throws Exception {
         device.setType(type);
         databaseDevices.updateDevice(device.getId(), device);
     }
 
-    //Tested
     public void deleteDevice(String deviceID) throws SQLException {
         databaseDevices.deleteFromDevices(deviceID);
     }
 
-    //Tested
     public void resetDevices() throws SQLException {
         List<Device> devices = getDevices();
         for (Device device : devices) {
             try {
                 databaseDevices.deleteFromDevices(device.getId());
+            } catch (SQLException e) {
             }
-            catch (SQLException e)
-            {}
         }
     }
 
-    //Tested
     public void deleteUserGroup(String group) throws SQLException {
         databaseUserGroups.deleteFromUserGroups(group);
     }
 
-    //Tested
     public void insertUserGroup(UserGroup group) throws SQLException {
         databaseUserGroups.insertIntoUserGroups(group);
     }
 
-    //Tested
     public UserGroup getUserGroup(String userGroupID) throws SQLException {
         return databaseUserGroups.loadUserGroup(userGroupID);
     }
 
-    //Tested
     public List<UserGroup> getUserGroups() throws SQLException {
         List<UserGroup> userGroups = new ArrayList<>();
         PreparedStatement stmt = databaseConnection.getConnection().prepareStatement("SELECT * FROM UserGroups");
@@ -235,22 +210,18 @@ public class WebBackend implements FromFrontEnd {
         return userGroups;
     }
 
-    //Tested
     public void insertRule(Rule rule) throws Exception {
         databaseRules.insertIntoRules(rule);
     }
 
-    //Tested
     public void deleteRule(String rule) throws Exception {
         databaseRules.deleteFromRules(rule);
     }
 
-    //Tested
     public Rule getRule(String ruleID) throws SQLException {
         return databaseRules.loadRule(ruleID);
     }
 
-    //Tested
     public List<Rule> getRules() throws SQLException {
         List<Rule> rules = new ArrayList<>();
         PreparedStatement stmt = databaseConnection.getConnection().prepareStatement("SELECT * FROM Rules");
@@ -267,27 +238,22 @@ public class WebBackend implements FromFrontEnd {
         return rules;
     }
 
-    //Tested
     public void insertGroupPermission(GroupPermission groupPermission) throws Exception {
         databaseUserGroups.insertIntoGroupPermissions(groupPermission);
     }
 
-    //Tested
     public void deleteGroupPermission(String groupPermissionID) throws Exception {
         databaseUserGroups.deleteFromGroupPermissions(groupPermissionID);
     }
 
-    //Tested
     public GroupPermission getGroupPermission(String ruleID, String userGroupID) throws SQLException {
-        return databaseUserGroups.loadGroupPermission(ruleID,userGroupID);
+        return databaseUserGroups.loadGroupPermission(ruleID, userGroupID);
     }
 
-    //Tested
     public GroupPermission getGroupPermission(String permissionID) throws SQLException {
         return databaseUserGroups.loadGroupPermission(permissionID);
     }
 
-    //Tested
     public List<GroupPermission> getGroupPermissions() throws SQLException {
         List<GroupPermission> permissions = new ArrayList<>();
         PreparedStatement stmt = databaseConnection.getConnection().prepareStatement("SELECT * FROM GroupPermissions");
@@ -304,16 +270,13 @@ public class WebBackend implements FromFrontEnd {
         return permissions;
     }
 
-    //Tested
-    public List<AssignmentHistory> getUserAssignmentHistory(String userID) throws SQLException{
+    public List<AssignmentHistory> getUserAssignmentHistory(String userID) throws SQLException {
         return databaseUsers.loadUserAssignmentHistory(userID);
     }
 
-    //Tested
     public List<AssignmentHistory> getDeviceAssignmentHistory(String deviceID) throws SQLException {
         return databaseDevices.loadDeviceAssignmentHistory(deviceID);
     }
-
 
     public List<User> parseUserCSV(MultipartFile csv) throws FileUploadException {
         Iterable<CSVRecord> records = multipartFileToRecords(csv);
@@ -358,7 +321,7 @@ public class WebBackend implements FromFrontEnd {
             newDevice.setType(record.get("scan value"));
             newDevice.setId(UUID.randomUUID().toString());
 
-        newDevices.add(newDevice);
+            newDevices.add(newDevice);
         }
 
         return newDevices;
@@ -380,7 +343,6 @@ public class WebBackend implements FromFrontEnd {
         }
     }
 
-    //Tested
     public String getUsersCSV() throws SQLException {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("id,");
@@ -410,7 +372,6 @@ public class WebBackend implements FromFrontEnd {
         return stringBuilder.toString();
     }
 
-    //Tested
     public String getDevicesCSV() throws SQLException {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("id,");
@@ -439,72 +400,63 @@ public class WebBackend implements FromFrontEnd {
         return stringBuilder.toString();
     }
 
-    //Tested
     public List<User> getGroupUsers(String userGroupID) throws SQLException {
         return databaseUsers.loadUsersFromUserGroup(userGroupID);
     }
 
-    //Tested
     public List<Rule> getGroupRules(String userGroupID) throws SQLException {
         return databaseUserGroups.loadGroupRules(userGroupID);
     }
 
-    //Tested
     public int getAvailableCount() throws SQLException {
         return databaseDevices.getAvailableCount();
     }
 
-    //Tested
     public int getTakenCount() throws SQLException {
         return databaseDevices.getTakenCount();
     }
 
-    //Tested
     public int getOtherCount() throws SQLException {
         return databaseDevices.getOtherCount();
     }
 
-    //Tested
     public List<Integer> getRecentTakeouts(int hoursToGet) throws SQLException {
 
         Calendar end = Calendar.getInstance();
         Calendar start = Calendar.getInstance();
         start.add(HOUR, -hoursToGet);
-        end.add(HOUR, -(hoursToGet-1));
+        end.add(HOUR, -(hoursToGet - 1));
 
         start.add(MONTH, 1);
         end.add(MONTH, 1);
 
         List<Integer> takeouts = new ArrayList<>();
 
-        for(int i = -(hoursToGet); i != 0; i++)
-        {
+        for (int i = -(hoursToGet); i != 0; i++) {
             takeouts.add(databaseAssignments.getAssignmentsTakeoutsByTime(start, end));
-            start.add(HOUR,1);
-            end.add(HOUR,1);
+            start.add(HOUR, 1);
+            end.add(HOUR, 1);
         }
 
         return takeouts;
     }
 
-    //Tested
     public List<Integer> getRecentReturns(int hoursToGet) throws SQLException {
 
         Calendar end = Calendar.getInstance();
         Calendar start = Calendar.getInstance();
         start.add(HOUR, -hoursToGet);
-        end.add(HOUR, -(hoursToGet-1));
+        end.add(HOUR, -(hoursToGet - 1));
 
         start.add(MONTH, 1);
         end.add(MONTH, 1);
 
         List<Integer> returns = new ArrayList<>();
 
-        for(int i = -(hoursToGet); i != 0; i++)
-        {
+        for (int i = -(hoursToGet); i != 0; i++) {
             returns.add(databaseAssignments.getAssignmentsReturnsByTime(start, end));
-            start.add(HOUR,1);
-            end.add(HOUR,1);
+            start.add(HOUR, 1);
+            end.add(HOUR, 1);
         }
 
         return returns;
@@ -512,80 +464,67 @@ public class WebBackend implements FromFrontEnd {
 
     public List<String> getTimes(int hoursToGet) {
         Calendar end = Calendar.getInstance();
-        end.add(HOUR, -(hoursToGet-1));
+        end.add(HOUR, -(hoursToGet - 1));
 
         end.add(MONTH, 1);
 
         List<String> times = new ArrayList<>();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
-        for(int i = -(hoursToGet); i != 0; i++)
-        {
+        for (int i = -(hoursToGet); i != 0; i++) {
             String time = sdf.format(end.getTime());
             times.add(time);
-            end.add(HOUR,1);
+            end.add(HOUR, 1);
         }
 
         return times;
     }
 
-    //Tested
     public List<UserGroup> getUserGroupsByRule(String ruleID) throws SQLException {
         return databaseUserGroups.loadGroupsByRule(ruleID);
     }
 
-    //Tested
     public List<Device> getDevicesByRule(String ruleID) throws SQLException {
         return databaseDevices.loadDevicesByRule(ruleID);
     }
 
-    //Tested
     public boolean userHasOutstandingDevices(String userID) throws SQLException {
         List<Device> devices = databaseDevices.loadOutstandingDevicesByUser(userID);
         return !devices.isEmpty();
     }
 
-    //Tested
     public void deleteAssignmentHistoryByUser(String userID) throws SQLException {
         databaseAssignments.deleteFromAssignmentHistoryByUser(userID);
     }
 
-    //Tested
     public boolean deviceIsOut(String deviceID) throws SQLException {
         return databaseDevices.deviceIsOut(deviceID);
     }
 
-    //Tested
     public void deleteAssignmentHistoryByDevice(String deviceID) throws SQLException {
         databaseAssignments.deleteFromAssignmentHistoryByDevice(deviceID);
     }
 
-    //Tested
     public void removeGroupFromUsers(String groupID) throws SQLException {
         databaseUsers.removeGroupFromUsers(groupID);
     }
 
-    //Tested
     public void deletePermissionsByGroup(String groupID) throws SQLException {
         databaseUserGroups.deletePermissionsByGroup(groupID);
     }
 
-    //Tested
     public void removeRuleFromDevices(String ruleID) throws SQLException {
         databaseDevices.removeRuleFromDevices(ruleID);
     }
 
-    //Tested
     public void deletePermissionsByRule(String ruleID) throws SQLException {
         databaseUserGroups.deletePermissionsByRule(ruleID);
     }
 
-    //Tested
     public boolean groupHasRule(String groupID, String ruleID) throws SQLException {
         return databaseRules.groupHasRule(groupID, ruleID);
     }
 
-    //Tested
     public void deletePermissions(List<GroupPermission> permissions) throws SQLException {
         databaseUserGroups.deletePermissions(permissions);
     }
